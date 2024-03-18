9f48b059bea3ab8568fbd9f0ca7a37bfa7fe9e30
Odusina Adeyemi Sodiq <odusina.adeyemi@gmail.com>
2024-03-18 12:22:37 +0100

    exp-tracker committed

---
+++ src/context/AppReducer.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state, action) => {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transaction: state.transaction.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transaction: [action.payload, ...state.transaction], 
+      };
+
+    default:
+      return state;
+  }
+};
